6fc465642b94f470b0aa7518bc4a7b09ccf81f61
lucas.m.silva_unifesp <lucas.silva16@unifesp.br>
2023-11-08 12:05:03 -0300

    adicao do hitbox no morcego

--- game_cappa/Characters/Characters.gd
+++ game_cappa/Characters/Characters.gd
@@ -3,9 +3,12 @@ class_name Character, "res://Assets/heroes/knight/knight_idle_anim_f0.png"
 
 const FRICTION: float = 0.15
 
+export(int) var hp: int = 2
+
 export(int) var accerelation: int = 40 
 export(int) var max_speed: int = 100
 
+onready var state_machine: Node = get_node("FiniteStateMachine")
 onready var animated_sprite: AnimatedSprite = get_node("AnimatedSprite")
 
 var mov_direction: Vector2 = Vector2.ZERO
@@ -19,3 +22,12 @@ func move() -> void:
 	mov_direction = mov_direction.normalized()
 	velocity = mov_direction * accerelation
 	velocity = velocity.clamped(max_speed)
+	
+func take_damage(dam: int, dir: Vector2, force: int) -> void:
+	hp -= dam
+	if hp > 0:
+		state_machine.set_state(state_machine.states.hurt)
+		velocity += dir * force
+	else:
+		state_machine.set_state(state_machine.states.dead)
+		velocity += dir * force * 2
---
+++ game_cappa/Characters/Enemies/Flying Creature/FlyingCreature.gd
@@ -0,0 +1,6 @@
+extends Enemy
+
+onready var hitbox: Area2D = get_node("Hitbox")
+
+func _process(_delta: float) -> void:
+	hitbox.knockback_direction = velocity.normalized()
