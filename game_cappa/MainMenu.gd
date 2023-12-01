b3ac944d4247554ca2a957716c0584132b496df8
joao-zip <oliveira.joao@unifesp.br>
2023-12-01 16:08:36 -0300

    added: tela de 'made by', porém sem botão de retornar ao menu principal

--- game_cappa/MainMenu.gd
+++ game_cappa/MainMenu.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 export var mainGameScene : PackedScene
-
+export var madeByScene : PackedScene
 
 
 func _on_New_Game_button_up():
 	get_tree().change_scene(mainGameScene.resource_path)
+
+
+func _on_Made_By_pressed():
+	get_tree().change_scene(madeByScene.resource_path)
